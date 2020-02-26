270c710b0ac95071f210e9c3fc246242331b02ad
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-02-26 16:31:46 +0530

    method for finding binary equivalent of given string is ready

--- src/execution/Helper.java
+++ src/execution/Helper.java
@@ -1,4 +1,34 @@
 package execution;
 
 public class Helper {
+    static void strToBinary(String s) {
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int val = s.charAt(i);
+            StringBuilder bin = new StringBuilder();
+            while (val > 0) {
+                if (val % 2 == 1) {
+                    bin.append('1');
+                } else
+                    bin.append('0');
+                val /= 2;
+            }
+            bin = new StringBuilder(reverse(bin.toString()));
+
+            System.out.print("0" + bin + " ");
+        }
+    }
+
+    static String reverse(String input) {
+        char[] a = input.toCharArray();
+        int l, r = 0;
+        r = a.length - 1;
+
+        for (l = 0; l < r; l++, r--) {
+            char temp = a[l];
+            a[l] = a[r];
+            a[r] = temp;
+        }
+        return String.valueOf(a);
+    }
 }
--- src/execution/Main.java
+++ src/execution/Main.java
@@ -2,9 +2,10 @@ package execution;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Helper {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.next();
+        strToBinary(s);
     }
 }
